fac06a25cdaa0b89448d1c9d91ad31f29532a29c
95A31 <x95a31x@gmail.com>
2014-01-07 13:29:38 +0100

    Update build commands

--- RecompileCM.sh
+++ RecompileCM.sh
@@ -1,5 +1,5 @@
 #Compile
-. build/envsetup.sh && brunch kumquat; 
+. build/envsetup.sh && brunch cm_kumquat-userdebug && make otapackage -j4;
 
 #Beep (3x)
 paplay /usr/share/sounds/freedesktop/stereo/complete.oga;
