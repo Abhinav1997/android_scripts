54c3eb2217aa0954da6b03abf6ec9b869f278ee9
95A31 <x95a31xreg@gmail.com>
2014-01-07 16:44:46 +0100

    More modular

--- CompileCM.sh
+++ CompileCM.sh
@@ -20,9 +20,4 @@ do
 done
 
 #Compile
-. build/envsetup.sh && brunch kumquat; 
-
-#Beep (3x)
-paplay /usr/share/sounds/freedesktop/stereo/complete.oga;
-paplay /usr/share/sounds/freedesktop/stereo/complete.oga;
-paplay /usr/share/sounds/freedesktop/stereo/complete.oga;
+./RecompileCM.sh
