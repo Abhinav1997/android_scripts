6b3ff7834bff739e8713836b6e33dcf839a5138e
95A31 <x95a31x@gmail.com>
2014-01-09 19:42:59 +0100

    Fix update command

--- CRDC.sh
+++ CRDC.sh
@@ -3,22 +3,22 @@
 ./repo forall -c 'git checkout `git rev-list --all -n1 --before="2014-01-06 15:00"`';
 
 pwd=$PWD
-git checkout private cm-11.0;
+git checkout cm-11.0;
 
 cd ./device/sony/kumquat;
-git checkout private cm-11.0;
+git checkout cm-11.0;
 
 cd $pwd; cd hardware/sem;
-git checkout private cm-11.0;
+git checkout cm-11.0;
 
 cd $pwd; cd kernel/sony/u8500;
-git checkout private cm-11.0;
+git checkout cm-11.0;
 
 cd $pwd; cd external/wpa_supplicant_8_ti;
-git checkout private cm-11.0;
+git checkout cm-11.0;
 
 cd $pwd; cd external/dbus;
-git checkout private cm-11.0
+git checkout cm-11.0
 
 cd $pwd;
 
