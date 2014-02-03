2802d68a489266c134a70634b4bf55ba07afd50a
95A31 <x95a31x@gmail.com>
2014-02-03 16:56:39 +0100

    Wi-Fi and Bluetooth made by Backports

---
+++ Modules.sh
@@ -0,0 +1,17 @@
+set -a
+  BACKPORT_DIR=hardware/semc/backports
+  CROSS_COMPILE=$(readlink -e prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin)/arm-eabi-
+  ARCH=arm
+  KERNEL_DIR=kernel/sony/u8500
+  KLIB_BUILD=out/target/product/kumquat/obj/KERNEL_OBJ
+  KLIB=out/target/product/kumquat/obj/KERNEL_OBJ
+set +a
+
+make clean;
+make defconfig-xperia;
+make;
+
+rmdir -rf hardware/semc/backports/modules &> /dev/null;
+mkdir hardware/semc/backports/modules;
+
+find -type f -name *.ko -exec mv {} hardware/semc/backports/modules \;
--- RecompileCM.sh
+++ RecompileCM.sh
@@ -1,7 +1,6 @@
 #Compile
 . build/envsetup.sh && brunch cm_kumquat-userdebug;
 
-#Beep (3x)
-paplay /usr/share/sounds/freedesktop/stereo/complete.oga;
-paplay /usr/share/sounds/freedesktop/stereo/complete.oga;
-paplay /usr/share/sounds/freedesktop/stereo/complete.oga;
+#Wi-Fi & Bluetooth
+
+./Modules.sh
