5d84e63cb6432dbe87992943d0c86948bd92aec5
95A31 <x95a31xreg@gmail.com>
2014-02-04 23:58:54 +0100

    Typooooo

--- Modules.sh
+++ Modules.sh
@@ -2,7 +2,7 @@ set -a
 BACKPORT_DIR=$(readlink -e hardware/semc/backports)
 CROSS_COMPILE=$(readlink -e prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin)/arm-eabi-
 ARCH=arm
-KERNEL_DIR= $(readlink -e kernel/sony/u8500)
+KERNEL_DIR=$(readlink -e kernel/sony/u8500)
 KLIB_BUILD=$(readlink -e  out/target/product/kumquat/obj/KERNEL_OBJ)
 KLIB=$(readlink -e out/target/product/kumquat/obj/KERNEL_OBJ)
 set +a
